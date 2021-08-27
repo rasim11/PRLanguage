16d01287752a27ae247d625d48bfb72590079149
Rasim Miralibekov <rami0721@netcracker.com>
2021-08-27 14:00:57 +0400

    Fresh project

---
+++ src/main/java/com/example/prlanguage/PrLanguageApplication.java
@@ -0,0 +1,13 @@
+package com.example.prlanguage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrLanguageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrLanguageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prlanguage/PrLanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prlanguage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrLanguageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
